6defc3af2140ef5aaffbfe78061265aa124d8876
Angel Macwan <47318130+angelmacwan@users.noreply.github.com>
2021-01-04 16:19:17 +0530

    Add files via upload

---
+++ precache-manifest.2367aeb34ac0fb6fec25e8f0ff996861.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1309819839eb99abbdaf55663c380722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a57fb768a89384e709a",
+    "url": "/static/css/2.64898e77.chunk.css"
+  },
+  {
+    "revision": "b6efa96ed2cfe93417f3",
+    "url": "/static/css/main.6c56383f.chunk.css"
+  },
+  {
+    "revision": "4a57fb768a89384e709a",
+    "url": "/static/js/2.b5ed8f41.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/static/js/2.b5ed8f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6efa96ed2cfe93417f3",
+    "url": "/static/js/main.4757f563.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e9a2f00d77caaf3bc4ec7f4df4ddba8b",
+    "url": "/static/media/CellularAutomaton.e9a2f00d.jpg"
+  },
+  {
+    "revision": "670798aaf4984a0dda40927f644e7d4a",
+    "url": "/static/media/chat.670798aa.jpg"
+  },
+  {
+    "revision": "5f3510144a4c26fd9e6f43d1997ec609",
+    "url": "/static/media/game.5f351014.jpg"
+  },
+  {
+    "revision": "08a4b54b604797b474c8f4efee2f7339",
+    "url": "/static/media/me.08a4b54b.jpg"
+  },
+  {
+    "revision": "d7214f5a2492d2f2b0e9d753eede46fc",
+    "url": "/static/media/poseNet.d7214f5a.jpg"
+  },
+  {
+    "revision": "ebbe3339e5898528ee1f4d792c01ed1e",
+    "url": "/static/media/regression.ebbe3339.jpg"
+  },
+  {
+    "revision": "061ff6aa4eb5484b98695ef76e89bbda",
+    "url": "/static/media/taskmanager.061ff6aa.jpg"
+  },
+  {
+    "revision": "74125dc0ab20da80d9a9955888f5d499",
+    "url": "/static/media/transfer.74125dc0.jpg"
+  },
+  {
+    "revision": "8d01958f75c90cc4f6ae604113838f32",
+    "url": "/static/media/yolo.8d01958f.jpg"
+  }
+]);
