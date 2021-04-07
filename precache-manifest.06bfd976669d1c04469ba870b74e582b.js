ff35f85490a900d90f1e29373de572dabad85f35
Angel Macwan <47318130+angelmacwan@users.noreply.github.com>
2021-04-07 13:57:03 +0530

    Add files via upload

---
+++ precache-manifest.06bfd976669d1c04469ba870b74e582b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4caecf99ab003f884fc0eae9e802752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d032f8c28989a427ff93",
+    "url": "/static/css/2.f6fd37eb.chunk.css"
+  },
+  {
+    "revision": "5eeb5f658606175e763f",
+    "url": "/static/css/main.c927ae4a.chunk.css"
+  },
+  {
+    "revision": "d032f8c28989a427ff93",
+    "url": "/static/js/2.78c9727c.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/static/js/2.78c9727c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eeb5f658606175e763f",
+    "url": "/static/js/main.04c20d9a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
